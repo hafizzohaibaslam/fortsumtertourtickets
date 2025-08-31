6e3222f08116525d349e134d19703f2635296d17
Zohaib Aslam <aamzzohaib@gmail.com>
2025-08-31 15:45:09 +0500

    added google ad manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -96,19 +96,19 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         {/* Google Tag Manager */}
-        {/* <Script
+        <Script
           id="gtm-head"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-KMZXDFMX');
+             function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-546MG5B8');
             `,
           }}
-        /> */}
+        />
 
         {/* Google Ads (gtag) – keep only if not fired via GTM */}
         <Script
@@ -134,14 +134,14 @@ export default function RootLayout({
         suppressHydrationWarning
       >
         {/* GTM noscript */}
-        {/* <noscript>
+        <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KMZXDFMX"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-546MG5B8"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript> */}
+          ></iframe>
+        </noscript>
 
         <Script
           id="tawk-to-widget"
